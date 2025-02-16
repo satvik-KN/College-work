46241904f195ac3de0a0fbe9c40fe1ad4cd705c2
Satvik219 <satvik21092005@gmail.com>
2025-02-16 11:36:23 +0530

    lab1done

---
+++ L1q3.java
@@ -0,0 +1,14 @@
+public class L1q3 {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = x;
+        // Implicit typecasting
+        System.out.println(y);
+
+        double a = 10.5;
+        int b = (int) a;
+        // Explicit typecasting
+        System.out.println(b);
+    }
+}
+
